95f87080bb800fe633d2be6a2b63eefb52370405
hanzhengliu <shenshi2017@outlook.com>
2018-11-07 15:29:21 +0800

    Adding news to module.

---
+++ modules/dirlister.py
@@ -0,0 +1,7 @@
+import os 
+
+def run(**arg):
+    print "[*] In dirlister module."
+    files = os.listdir('.')
+
+    return str(files)
---
+++ modules/environment.py
@@ -0,0 +1,5 @@
+import os 
+
+def run(**arg):
+    print "[*] In environment module."
+    return str(os.environ)
